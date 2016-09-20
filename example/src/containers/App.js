c58e175f1d33e49ba0aab1e1d32667498842fa22
isto <hel.isto.d@gmail.com>
2016-09-20 20:33:42 +0400

    Add Babel + config for ES6 & add react & add component input for testing in sample

--- example/src/containers/App.js
+++ example/src/containers/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import { Input } from '../../../'
+
 class App extends Component {
   render() {
     return (
-      <div className="ui"></div>
+      <div className="ui">
+        <Input type='text' />
+      </div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+var paths = require('./config/paths')
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		path: './dist',
+		path: './build',
 		filename: 'Zambrocal.js',
 		libraryTarget: 'umd',
 		library: 'zambrocal-ui-react',
 		umdNamedDefine: true
+	},
+	module: {
+		loaders: [
+			{	// Process JS with Babel
+				test: /\.(js|jsx)$/,
+				loader: 'babel',
+				include: paths.appSrc,
+				query: require('./config/babel')
+			}
+		]
 	}
 }
